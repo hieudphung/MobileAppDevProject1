19c2a31c413d2996edd2177a2579c5b0b1091c51
Hieu Phung <142374885+hieudphung@users.noreply.github.com>
2024-06-27 15:35:26 -0400

    Goals card UI

--- finance_app/lib/pages/goalpage.dart
+++ finance_app/lib/pages/goalpage.dart
@@ -1,12 +1,80 @@
 import 'package:flutter/material.dart';
-
 import '../database/finance_tables.dart';
-
 import '../model/goal.dart';
-import '../widget/goal_card.dart';
+
+class GoalCard extends StatelessWidget {
+  final int goalId;
+  final String name;
+  final int goalType;
+  final int goalCurrent;
+  final int goalTarget;
+
+  const GoalCard({
+    required this.goalId,
+    required this.name,
+    required this.goalType,
+    required this.goalCurrent,
+    required this.goalTarget,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              children: [
+                Expanded(
+                  child: Text(
+                    name,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+                Text(
+                  '\$${goalCurrent.toString()}/\$${goalTarget.toString()}',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.arrow_forward),
+                  onPressed: () {
+                    // Handle arrow button action
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    // Handle delete button action
+                  },
+                ),
+              ],
+            ),
+            SizedBox(height: 10),
+            LinearProgressIndicator(
+              value: goalCurrent / goalTarget,
+              backgroundColor: Colors.grey[300],
+              color: Colors.blue,
+              minHeight: 8,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class GoalsScreen extends StatelessWidget {
-  const GoalsScreen({super.key,
+  const GoalsScreen({
+    super.key,
     required this.goalData,
   });
 
@@ -14,38 +82,34 @@ class GoalsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ListTile(
-          title: Text('Finance Goals'),
-          trailing: IconButton(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Goals'),
+        actions: [
+          IconButton(
             icon: Icon(Icons.add),
             onPressed: () {
               _showAddGoalDialog(context);
             },
           ),
-        ),
-
-        /*
-        Expanded(
-          child: ListView.builder(
-            itemCount: goalData.length, // replace with your goals count
-            itemBuilder: (context, index) {
-              return GoalCard();
-            },
-          ),
-        ),
-        */
-
-        Flexible(
-          child: 
-              ListView.builder(
-          itemCount: goalData.length,
-          itemBuilder: (_,int index) => GoalCard(goalId: goalData[index].id!, name: goalData[index].name, goalType: goalData[index].goalType, goalCurrent: goalData[index].goalCurrent, goalTarget: goalData[index].goalTarget),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: ListView.builder(
+              itemCount: goalData.length,
+              itemBuilder: (_, int index) => GoalCard(
+                goalId: goalData[index].id!,
+                name: goalData[index].name,
+                goalType: goalData[index].goalType,
+                goalCurrent: goalData[index].goalCurrent,
+                goalTarget: goalData[index].goalTarget,
+              ),
             ),
-        )
-        //*/
-      ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -63,7 +127,7 @@ class GoalsScreen extends StatelessWidget {
                 Navigator.of(context).pop();
               },
             ),
-            TextButton(
+            ElevatedButton(
               child: Text('Add'),
               onPressed: () {
                 // Handle adding new goal
@@ -76,27 +140,6 @@ class GoalsScreen extends StatelessWidget {
     );
   }
 }
-
-/*
-class GoalCard extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(10.0),
-      child: ListTile(
-        title: Text('Goal Name'),
-        subtitle: Text('Total Amount: \$1000'),
-        trailing: TextButton(
-          child: Text('Pay'),
-          onPressed: () {
-            // Handle pay towards goal
-          },
-        ),
-      ),
-    );
-  }
-}
-*/
 
 class AddGoalForm extends StatefulWidget {
   @override
@@ -114,60 +157,75 @@ class _AddGoalFormState extends State<AddGoalForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Goal Name'),
-            validator: (value) {
-              if (value!.isEmpty) {
-                return 'Please enter a goal name';
-              }
-              return null;
-            },
-            onSaved: (value) {
-              _goalName = value!;
-            },
-          ),
-          DropdownButtonFormField<String>(
-            value: _goalType,
-            items: <String>['Saving', 'Investment', 'Miscellaneous'].map((String value) {
-              return DropdownMenuItem<String>(
-                value: value,
-                child: Text(value),
-              );
-            }).toList(),
-            decoration: InputDecoration(labelText: 'Goal Type'),
-            onChanged: (value) {
-              setState(() {
-                _goalType = value!;
-              });
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Description'),
-            onSaved: (value) {
-              _description = value!;
-            },
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Target Amount'),
-            keyboardType: TextInputType.number,
-            onSaved: (value) {
-              int newAmount = int.parse(value!);
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: 'Goal Name'),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter a goal name';
+                  }
+                  return null;
+                },
+                onSaved: (value) {
+                  _goalName = value!;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButtonFormField<String>(
+                value: _goalType,
+                items: <String>['Saving', 'Investment', 'Miscellaneous']
+                    .map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                decoration: InputDecoration(labelText: 'Goal Type'),
+                onChanged: (value) {
+                  setState(() {
+                    _goalType = value!;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                onSaved: (value) {
+                  _description = value!;
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(labelText: 'Target Amount'),
+                keyboardType: TextInputType.number,
+                onSaved: (value) {
+                  int newAmount = int.parse(value!);
 
-              goalAmount = newAmount;
+                  goalAmount = newAmount;
 
-              if (newAmount < 100) {
-                goalAmount = 100;
-              }
+                  if (newAmount < 100) {
+                    goalAmount = 100;
+                  }
 
-              if (newAmount > 10000) {
-                goalAmount = 10000;
-              }
-            },
-          ),
-        ],
+                  if (newAmount > 10000) {
+                    goalAmount = 10000;
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
