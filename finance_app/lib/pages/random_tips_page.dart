a1d4afca4614765f069d075d7bdc3ab3f8a2ceec
Makda Solomon <abebe_makeda@yahoo.com>
2024-06-29 04:52:55 -0400

    this page is now functional and allows users to receive random tips by pressing the "Show Another Tip" button.

--- finance_app/lib/pages/random_tips_page.dart
+++ finance_app/lib/pages/random_tips_page.dart
@@ -1,6 +1,40 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class RandomTipsPage extends StatelessWidget {
+class RandomTipsPage extends StatefulWidget {
+  @override
+  _RandomTipsPageState createState() => _RandomTipsPageState();
+}
+
+class _RandomTipsPageState extends State<RandomTipsPage> {
+  final List<String> tips = [
+    'Save a portion of your income each month.',
+    'Create and stick to a budget.',
+    'Invest in a diversified portfolio.',
+    'Avoid high-interest debt.',
+    'Track your expenses regularly.',
+    'Plan for retirement early.',
+    'Build an emergency fund.',
+    'Live below your means.',
+    'Take advantage of employer matching in retirement accounts.',
+    'Educate yourself about personal finance.'
+  ];
+
+  String randomTip = '';
+
+  @override
+  void initState() {
+    super.initState();
+    generateRandomTip();
+  }
+
+  void generateRandomTip() {
+    final random = Random();
+    setState(() {
+      randomTip = tips[random.nextInt(tips.length)];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +42,21 @@ class RandomTipsPage extends StatelessWidget {
         title: Text('Random Tips'),
       ),
       body: Center(
-        child: Text('Random Tips Page Content'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              randomTip,
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20.0),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              onPressed: generateRandomTip,
+              child: Text('Show Another Tip'),
+            ),
+          ],
+        ),
       ),
     );
   }
